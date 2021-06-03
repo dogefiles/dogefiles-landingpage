96a3f414910d4c45302eaf9086696c0609bd9299
Robin Singh <42088724+iamrobins@users.noreply.github.com>
2021-06-03 22:02:25 +0530

    Update index.js

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -100,7 +100,8 @@ export const getStaticProps = async context => {
 export const getStaticPaths = async () => {
   try {
     const res = await fetch(
-      `${blogUrl}/ghost/api/v3/content/posts/?key=${contentApiKey}`
+//       `${blogUrl}/ghost/api/v3/content/posts/?key=${contentApiKey}`
+      https://test-ghost-99.herokuapp.com/ghost/api/v3/content/posts/?key=a21555e1e5e6b60ce4217ebbd6
     );
 
     const posts = await res.json();
@@ -109,7 +110,7 @@ export const getStaticPaths = async () => {
     const paths = ids.map(id => ({ params: { id: id.toString() } }));
 
     return {
-      paths,
+      paths, fallback: false
     };
   } catch (err) {
     return { paths: [], fallback: false };
