5a8bb50918a3a96a3ef36fb9a6099f1de8828791
Robin Singh <42088724+iamrobins@users.noreply.github.com>
2021-06-03 22:16:00 +0530

    Update index.js

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -84,9 +84,8 @@ export default function blog_post({ post }) {
 }
 
 export const getStaticProps = async context => {
-  const res = await fetch(
-    `${blogUrl}/ghost/api/v3/content/posts/${context.params.id}/?key=${contentApiKey}`
-  );
+  //`${blogUrl}/ghost/api/v3/content/posts/${context.params.id}/?key=${contentApiKey}`
+  const res = await fetch("https://test-ghost-99.herokuapp.com/ghost/api/v3/content/posts/?key=a21555e1e5e6b60ce4217ebbd6");
 
   const data = await res.json();
   const post = data.posts[0];
