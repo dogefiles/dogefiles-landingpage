d19728cffce4fb2cdf974c7339712f941ccf8f7d
hariomkushwaha <hari2018cs048abesit@gmail.com>
2021-06-13 14:13:49 +0530

    about us page created

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,83 @@
-import { Text } from "@chakra-ui/react";
+import { Box,Heading,Text,Avatar, 
+  Center } from "@chakra-ui/react"
 
-export default function about_us() {
-  return <Text>About Us Page</Text>;
-}
+const AboutUs = () => {
+  return (
+    <Box paddingTop="8px">
+    <Center  h="100px" >
+    <Heading as="h3" size="lg">
+    About us
+  </Heading>
+</Center>
+        <Box align={["center","left"]} p={5}>
+        <Box
+        p={5}
+        shadow="md"
+        borderWidth="1px"
+        flex="1"
+        borderRadius="md"
+        width={["100%","45%"]}
+      >
+      <Avatar src="https://bit.ly/broken-link" float="right"/>
+        <Heading fontSize="xl" align="left">title</Heading>
+        
+        <Text mt={4} align="left">desc</Text>
+        
+      </Box>
+      </Box>
+
+
+        <Box align="right" p={5}>
+        <Box
+        p={5}
+        shadow="md"
+        borderWidth="1px"
+        flex="1"
+        borderRadius="md"
+        width={["100%","45%"]}
+      >
+      <Avatar src="https://bit.ly/broken-link" float="right"/>
+        <Heading fontSize="xl" align="left">title</Heading>
+        <Text mt={4} align="left">desc</Text>
+    
+      </Box>
+        </Box>
+
+
+          <Box align={["center","left"]} p={5}>
+          <Box
+          p={5}
+          shadow="md"
+          borderWidth="1px"
+          flex="1"
+          borderRadius="md"
+          width={["100%","45%"]}
+        >
+        <Avatar src="https://bit.ly/broken-link" float="right"/>
+          <Heading fontSize="xl">title</Heading>
+          <Text mt={4}>desc</Text>
+      
+        </Box>
+          </Box>
+
+
+          <Box align="right" p={5}>
+        <Box
+        p={5}
+        shadow="md"
+        borderWidth="1px"
+        flex="1"
+        borderRadius="md"
+        width={["100%","45%"]}
+      >
+      <Avatar src="https://bit.ly/broken-link" float="right"/>
+        <Heading fontSize="xl" align="left">title</Heading>
+        <Text mt={4} align="left">desc</Text>
+    
+      </Box>
+        </Box>
+    </Box>
+  );
+};
+
+export default AboutUs;
