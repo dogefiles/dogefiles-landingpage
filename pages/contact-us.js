11492e6f950eb3e54d04f3fda67b3e1a920b05a0
hariomkushwaha <hari2018cs048abesit@gmail.com>
2021-06-16 19:30:40 +0530

    embeded

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,5 +1,7 @@
-import { Text } from "@chakra-ui/react";
+import { Text,Box } from "@chakra-ui/react";
 
 export default function contact_us() {
-  return <Text>Contact Us Page</Text>;
+  return <Box >
+  <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSfbytWw4v6Dr-57Yepmjv0iVN8aoq0RzYOPdetgmELVTIJBfQ/viewform?embedded=true" width={["100%","80%"]} height="991px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  </Box>;
 }
