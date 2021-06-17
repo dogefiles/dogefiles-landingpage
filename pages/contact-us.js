dddb9bea434bf720de28b20fec54c8c02e1b59aa
hariomkushwaha <hari2018cs048abesit@gmail.com>
2021-06-17 15:37:38 +0530

    added testimonials

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,7 +3,9 @@ import { Text,Box } from "@chakra-ui/react";
 export default function contact_us() {
   return <Box>
   <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSfbytWw4v6Dr-57Yepmjv0iVN8aoq0RzYOPdetgmELVTIJBfQ/viewform?embedded=true" width={["100%","80%"]}
-   height="991px" frameborder="0" marginheight="0"
-    marginwidth="0">Loading…</iframe>
+  height="1080px"
+  frameBorder="0"
+  marginHeight="0"
+  marginWidth="0">Loading…</iframe>
   </Box>;
 }
