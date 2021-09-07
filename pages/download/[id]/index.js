eef7f970ea392c980993e481cd228e41e7d81dbb
Robin Singh <itsrobin4@gmail.com>
2021-09-07 19:21:45 +0530

    Download Page Error handling improved

--- pages/download/[id]/index.js
+++ pages/download/[id]/index.js
@@ -3,8 +3,12 @@ import DownloadCard from "components/DownloadCard";
 import Page from "components/layout/Page";
 import nameFormatter from "utils/nameFormatter";
 import { SERVER_URL } from "global/envs";
+import Error from "next/error";
 
-export default function download({ file, user }) {
+export default function download({ errorCode, data }) {
+  if (errorCode !== 200) return <Error statusCode={errorCode} />;
+
+  const { file, user } = data;
   const fileName = file.fileName && nameFormatter(file.fileName);
 
   return (
@@ -32,15 +36,25 @@ export default function download({ file, user }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-  const res = await fetch(`${SERVER_URL}/S3/objectInfo/${id}`);
+export async function getServerSideProps({ params }) {
+  const { id } = params;
+  let errorCode;
+  let data;
+  try {
+    const response = await fetch(`${SERVER_URL}/S3/objectInfo/${id}`);
+    errorCode = response.status;
+    data = await response.json();
+    if (errorCode !== 200) {
+      data = null;
+    }
+  } catch (err) {
+    data = null;
+  }
 
-  const data = await res.json();
   return {
     props: {
-      file: data.file,
-      user: data.user,
+      errorCode,
+      data,
     },
   };
 }
