d31a77726492a94406f2b8e6ba5a7149548ffbdb
Robin Singh <42088724+iamrobins@users.noreply.github.com>
2023-03-07 19:13:45 +0530

    added redirect rule for ads.txt

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
     domains: ["static.ghost.org", "images.unsplash.com"],
   },
   target: "serverless",
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/46379/dogefiles.io',
+        permanent: true,
+      },
+    ]
+  }
 };
